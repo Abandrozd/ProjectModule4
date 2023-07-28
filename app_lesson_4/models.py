2a4f06a514737c03012ee752ee0a7ebf42fca748
Abandrozd <58365637+Abandrozd@users.noreply.github.com>
2023-07-28 19:14:37 +0300

    Add files via upload

---
+++ app_lesson_4/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# заголовок
+# описание
+# цена
+# дата создания
+# дата обновления
+# торг
+
+class Advert(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title, self.description, self.price}'
+
+    class Meta:
+        db_table = 'Advertisements'
