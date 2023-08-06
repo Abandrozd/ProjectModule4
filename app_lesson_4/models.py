3446959e43f9895e058ba3193813f4b41313de57
Abandrozd <58365637+Abandrozd@users.noreply.github.com>
2023-08-06 22:01:16 +0300

    Add files via upload

---
+++ app_lesson_4/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
+
+class Advert(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def date_of_creation(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def date_of_update(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    def __str__(self):
+        return f'{self.title}, {self.price}'
+
+    class Meta:
+        db_table = 'Advertisements'
---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from app_lesson_4.views import index, top_sellers
+
+
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('admin/', admin.site.urls)
+]
